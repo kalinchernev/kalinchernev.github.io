434081f5a6c45eea7ede4864c1e59601f824d8c2
Kalin Chernev <kalin.chernev@gmail.com>
2017-04-19 09:39:40 +0200

    A bit of scripting

---
+++ sw-precache-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  staticFileGlobs: ['public/**/*.{js,html,css,png,jpg,gif,eot,svg,ttf,woff,woff2,otf}'],
+  stripPrefix: 'public',
+  swFilePath: 'public/js/service-worker.js',
+};
