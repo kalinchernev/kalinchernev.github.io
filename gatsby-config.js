28d97eaf94fb9d4477ce346a31f2ae1413670cbe
Kalin Chernev <kalin.chernev@gmail.com>
2018-04-09 17:54:20 +0200

    For preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,18 +56,7 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#ffffff',
         display: 'standalone',
-        icons: [
-          {
-            src: './favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: './favicons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        icon: './src/assets/images/favicon.png',
       },
     },
     `gatsby-plugin-offline`,
