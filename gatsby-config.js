22b36c328b5cb856b213238bb4ae91782aeb8420
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-21 11:57:40 +0200

    Continue refactoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 740,
+              maxWidth: 960,
             },
           },
           {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
-import styles from '../components/css/styles.module.css';
 
-export default () => (
-  <div>
-    <Helmet htmlAttributes={{ lang: `en` }}>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0 shrink-to-fit=no"
-      />
-      <meta
-        name="google-site-verification"
-        content="M3xN17b8dlWWV8ecUcM0iE7INS1WSbBYbDADkVpFElM"
-      />
-      <title>Kalin Chernev Personal Website</title>
-    </Helmet>
-  </div>
-);
+export default ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
+  return (
+    <div>
+      <Helmet htmlAttributes={{ lang: 'en' }}>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0 shrink-to-fit=no"
+        />
+        <meta
+          name="google-site-verification"
+          content="M3xN17b8dlWWV8ecUcM0iE7INS1WSbBYbDADkVpFElM"
+        />
+        <title>Kalin Chernev Personal Website</title>
+      </Helmet>
+      <ul styles={{ listStyle: 'none' }}>
+        {posts.map(({ node }) => {
+          const post = node.frontmatter;
+          return (
+            <li>
+              <Link to={post.slug}>
+                <h2>{post.title}</h2>
+                <p>{node.excerpt}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      limit: 10
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          frontmatter {
+            title
+            slug
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`;
