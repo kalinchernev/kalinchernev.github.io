769b142a83c333af9cd17511bd546a15a66a8b87
Kalin Chernev <kalin.chernev@gmail.com>
2018-05-21 23:20:08 +0200

    Preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               backgroundColor: 'transparent',
-              maxWidth: 960,
+              maxWidth: 720,
             },
           },
           {
